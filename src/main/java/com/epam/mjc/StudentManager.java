7c458944ca4f750e2e5c90791c290caaf946f3d0
azimjonalijonov <115147667+azimjonalijonov@users.noreply.github.com>
2022-12-15 18:30:25 +0500

    Update StudentManager.java

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -1,3 +1,4 @@
+
 package com.epam.mjc;
 
 
@@ -6,7 +7,11 @@ public class StudentManager {
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
   public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+    if(Student.getValueOf(studentID)!=null){
+      return Student.getValueOf(studentID);
+    } else {
+      throw new StudentNotFound("Could not find student with ID "+studentID);
+    }
   }
 
   public static void main(String[] args) {
