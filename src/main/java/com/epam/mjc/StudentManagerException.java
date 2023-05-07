b7fd60b9084a71aefaf28f40b1df0d268e5b4cce
azimjonalijonov <115147667+azimjonalijonov@users.noreply.github.com>
2023-05-07 18:30:18 +0500

    Update StudentManagerException.java

--- src/main/java/com/epam/mjc/StudentManagerException.java
+++ src/main/java/com/epam/mjc/StudentManagerException.java
@@ -1,1 +1,12 @@
 
+package com.epam.mjc;
+
+
+public class StudentManagerException extends IllegalArgumentException {
+
+    public StudentManagerException(String message) {
+        super(message);
+    }
+
+
+}
